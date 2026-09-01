5d9b5b7d18d5c4c6af869fb61beb38396d5600e4
Akshay <baikadiakshaykumar@gmail.com>
2026-09-01 22:29:52 +0530

    added some changes in html and added script.js file for functionality test

---
+++ Responsive/script.js
@@ -0,0 +1,48 @@
+// var planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"];
+// console.log(planets);
+// console.log(planets[0]);
+// console.log(planets[0] [0]);
+
+var name1 = "Mercury"
+var name2 = "Venus"
+var name3 = "Earth"
+var name4 = "Mars"
+var name5 = "Jupiter"
+var name6 = "Saturn"
+var name7 = "Uranus"
+var name8 = "Neptune"
+
+// console.log(name1);
+// console.log(name1);
+// console.log(name1);
+// console.log(name1);
+// console.log(name1);
+// console.log(name1);
+// console.log(name1);
+// console.log(name1);
+
+// for (let i=0;i<=8;i++){
+//     console.log("printing the vaule using the loops concepts");
+//     console.log(name1);
+// }
+
+// for (let i=0;i<=6;i++) {
+//     console.log(name1[i]);
+// }
+
+for (let i = 0; i <= 6; i + 3) {
+    console.log(name1[i]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
